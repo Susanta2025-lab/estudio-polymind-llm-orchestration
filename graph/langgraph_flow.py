7ab6c20175d741f386d7da776cd80075c6912c20
Susanta Hazra <h.susanta@gmail.com>
2026-06-02 18:46:18 +0100

    phase 4 completed

--- graph/langgraph_flow.py
+++ graph/langgraph_flow.py
@@ -1,25 +1,34 @@
 from langgraph.graph import StateGraph
 from graph.state import GraphState
 
-from graph.nodes import router_node, direct_llm_node, rag_node, tool_node
+from graph.nodes import router_node, model_router_node, direct_llm_node, rag_node, tool_node
 
 # Initialize the graph builder
 builder = StateGraph(GraphState)
 
 # Add nodes to the graph
 builder.add_node("router", router_node)
+builder.add_node("model_router", model_router_node)
 builder.add_node("direct", direct_llm_node)
 builder.add_node("rag", rag_node)
 builder.add_node("tool", tool_node)
+
+
 
 #define routing logic
 def route_decision(state):
 
     return state["route"]
 
+
+builder.add_edge(
+    "router",
+    "model_router"
+)
+
 #Conditional edges based on routing logic
 builder.add_conditional_edges(
-    "router",
+    "model_router",
     route_decision,
     {
         "rag": "rag",
